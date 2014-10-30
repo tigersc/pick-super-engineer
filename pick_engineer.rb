b2db9a7ef3258ae281a42b2a3139df01e1415496
VarakornChinvarakorn <ein2537@hotmail.com>
2014-10-30 12:11:36 +0700

    Update pick_engineer.rb

--- pick_engineer.rb
+++ pick_engineer.rb
@@ -26,19 +26,19 @@ def featureA(engineer)
 	return a
 end
 
-def featureB(data)
-	#puts data 			# print name of engineer with random 
-	a = data.split(' ') 		# convert string to an array of characters with the split method.
-	b = Array.new 			# create new array => []
-	b = a[1].split("") 		# convert string into array, split on an empty string delimiter.
-	puts a[0]+' '+b[0] +'...' 	# show output-------
-end
+def featureB(engineer)  
+	# convert string to an array of characters with the split method. 
+	a = engineer.split(' ') 	 
+	b = Array.new 			# create new array => [] 
+	# convert string into array, split on an empty string delimiter. 
+	b = a[1].split("") 		 
+	return a[0]+' '+b[0] +'...' 	# show output------- 
+end 
+
 
 name = pick_engineer()
 puts "1. %s" % [name]
 
-name = featureA(name)
-puts "2. %s" % [name]
+puts "2. %s" % [featureA(name)]
 
-name = featureB(name)
-puts "3. %s" % [name]
+puts "3. %s" % [featureB(name)]
